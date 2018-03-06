7990bfc8ad858fead47db92f56ae74558a313aa2
MortenFr <36443440+MortenFr@users.noreply.github.com>
2018-03-06 13:56:07 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 
 #define GPIO ((NRF_GPIO_REGS*)0x50000000)
 
@@ -11,7 +12,7 @@ typedef struct {
 	volatile uint32_t DIR;
 	volatile uint32_t DIRSET;
 	volatile uint32_t DIRCLR;
-	volatile uint32_t RESERVED1[121];
+	volatile uint32_t RESERVED1[120];
 	volatile uint32_t PIN_CNF[32];
 } NRF_GPIO_REGS;
 
@@ -23,17 +24,36 @@ int main(){
 	}
 
 	// Configure buttons
-	GPIO->PIN_CNF[__BUTTON_A_PIN__] = 0;
-	GPIO->PIN_CNF[__BUTTON_B_PIN__] = 0;
+	GPIO->PIN_CNF[17] = 0;
+	GPIO->PIN_CNF[26] = 0;
 
 	int sleep = 0;
 	while(1){
 
-		/* Check if button B is pressed;
-		 * turn on LED matrix if it is. */
+		if (!(GPIO->IN&(1 << 26)))
+		{
+		 for (int high = 13; high < 16; high++)
+		 {
+		 	GPIO->OUTSET = (1 << high);
+		 }
+		 for (int low = 4; low < 13; low++)
+		 {
+		 	GPIO->OUTCLR = (1 << low);
+		 }
+		}
 
-		/* Check if button A is pressed;
-		 * turn off LED matrix if it is. */
+
+		if (!(GPIO->IN&(1 << 17)))
+		{
+		 for (int high = 13; high < 16; high++)
+		 {
+		 	GPIO->OUTCLR = (1 << high);
+		 }
+		 for (int low = 4; low < 13; low++)
+		 {
+		 	GPIO->OUTSET = (1 << low);
+		 }
+		}
 
 		sleep = 10000;
 		while(--sleep);
